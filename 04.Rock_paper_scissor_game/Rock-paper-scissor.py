6972aeb7f748c58d06188757e1de796dc7178484
Pedro Costa <60372240+pedrolocosta@users.noreply.github.com>
2026-05-19 23:59:57 -0300

    Create Rock-paper-scissor.py

---
+++ 04.Rock_paper_scissor_game/Rock-paper-scissor.py
@@ -0,0 +1,22 @@
+import random
+
+def get_choices():
+    player_choice = input("Enter a choice (rock, paper, scissors): ")
+    options = ["rock", "paper", "scissor"]
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
+
+def check_win(player, computer):
+    print(f"You chose {player}, computer chose {computer}")
+    if player == computer:
+        return"it's a tie!"
+    elif player == "rock" and computer =="scissor":
+        return "Rock smashes scissors!you win!"
+    elif player == "rock" and computer == "paper":
+        return "Paper covers rocks! You lose."
+
+check_win("rock", "paper")
+
+https://www.youtube.com/watch?v=eWRfhZUzrAc
+at 34:30 I stopped the study.
